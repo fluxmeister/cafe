5379c861ce6cda5707c714e177e69c9536829d3d
FluxMeister <doctorpauk@gmail.com>
2021-12-03 02:17:20 +0100

    Re-initialization

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'vektorwe_cafe' );
+define( 'DB_NAME', 'cafe' );
 
 /** MySQL database username */
-define( 'DB_USER', 'vektorwe_cafe' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Jx3NdpGpox6RNEuo' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
